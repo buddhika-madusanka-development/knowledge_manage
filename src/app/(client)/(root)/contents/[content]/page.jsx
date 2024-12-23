c9781e17491e9d6a378292bcaa128be51ecd68f8
Spider Man <peter@gmail.com>
2024-12-23 18:41:49 +0530

    Complete content display area in single content area

--- src/app/(client)/(root)/contents/[content]/page.jsx
+++ src/app/(client)/(root)/contents/[content]/page.jsx
@@ -1,4 +1,6 @@
-import getUrlPathname from "@/lib/getUrlPathname";
+import Tag from "@/components/common/Tag";
+import HighliteTitle from "@/components/common/Titles/HighliteTitle";
+import RelatedContentSection from "@/components/UI/SingleContent/RelatedContentSection";
 import React from "react";
 
 const page = async ({params}) => {
@@ -12,29 +14,52 @@ const page = async ({params}) => {
   if(contentRes.ok){
     content = await contentRes.json();
   }
-
-  console.log(content?.contentTitle);
   
   return (
-    <div className="w-full flex flex-col gap-y-5 md:flex-row md:gap-x-8 mt-10">
-      {/* video section */}
-      <div className="w-full md:w-2/3">
-        <iframe
-          src="https://www.youtube.com/embed/Hej48pi_lOc?si=E9aTOa_VOQyvP_lf"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-          className="w-full aspect-video rounded-2xl"
-        ></iframe>
+
+    <div>
+
+      {/* Content area */}
+      <div className="w-full flex flex-col gap-y-5 md:flex-row md:gap-x-8 mt-10">
+        {/* video section */}
+        <div className="w-full md:w-2/3">
+          <iframe
+            src="https://www.youtube.com/embed/Hej48pi_lOc?si=E9aTOa_VOQyvP_lf"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+            className="w-full aspect-video rounded-2xl"
+          ></iframe>
+        </div>
+
+        {/* content section */}
+        <div className="w-full md:w-1/3 md:py-10">
+          <h1 className="text-2xl md:text-4xl font-bold">{content?.contentTitle}</h1>
+
+          <div className="flex flex-wrap gap-2 mt-2 md:mt-4">
+            {
+              content?.Tags.map((tag) => (
+                <Tag key={tag} tag={tag} />
+              ))
+            }
+          </div>
+          
+          <p className="text-sm text-mute mt-6">{content?.contentDescription}</p>
+        </div>
       </div>
 
-      {/* content section */}
-      <div className="w-full md:w-1/3">
-        <h1 className="text-2xl font-bold">{content?.contentTitle}</h1>
-        <p className="text-sm">{content?.contentDescription}</p>
+
+      {/* related content section */}
+      <div className="mt-16">
+        <HighliteTitle title={'Related'} hightliteText={'Contents'} />
+        <div className="contents">
+          <RelatedContentSection />
+        </div>
       </div>
+      
     </div>
+    
   );
 };
 
