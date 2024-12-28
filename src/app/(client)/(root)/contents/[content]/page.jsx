a16f797a7e47ca478591f588955142f36cd8aa2d
Spider Man <peter@gmail.com>
2024-12-28 14:45:38 +0530

    Fix build issue occour from the content page

--- src/app/(client)/(root)/contents/[content]/page.jsx
+++ src/app/(client)/(root)/contents/[content]/page.jsx
@@ -1,3 +1,4 @@
+import ContentNotFoundError from "@/components/common/ContentNotFoundError";
 import Tag from "@/components/common/Tag";
 import HighliteTitle from "@/components/common/Titles/HighliteTitle";
 import RelatedContentSection from "@/components/UI/SingleContent/RelatedContentSection";
@@ -5,60 +6,81 @@ import React from "react";
 
 const page = async ({params}) => {
 
+  let availability = false;
   const id = (await params).content;
-  console.log(id)
 
   const contentRes = await fetch(`http://localhost:3000/api/content/${id}`);
   let content = null;
 
   if(contentRes.ok){
     content = await contentRes.json();
+    availability = true;
   }
   
   return (
 
-    <div>
+    availability ? (
 
-      {/* Content area */}
-      <div className="w-full flex flex-col gap-y-5 md:flex-row md:gap-x-8 mt-10">
-        {/* video section */}
-        <div className="w-full md:w-2/3">
-          <iframe
-            src={content?.contentLink}
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
-            className="w-full aspect-video rounded-2xl"
-          ></iframe>
-        </div>
+      <div>
 
-        {/* content section */}
-        <div className="w-full md:w-1/3 md:py-10">
-          <h1 className="text-2xl md:text-4xl font-bold">{content?.contentTitle}</h1>
-
-          <div className="flex flex-wrap gap-2 mt-2 md:mt-4">
-            {
-              content?.Tags.map((tag) => (
-                <Tag key={tag} tag={tag} />
-              ))
-            }
+        {/* Content area */}
+        <div className="w-full flex flex-col gap-y-5 md:flex-row md:gap-x-8 mt-10">
+          {/* video section */}
+          <div className="w-full md:w-2/3">
+            <iframe
+              src={content?.contentLink}
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              className="w-full aspect-video rounded-2xl"
+            ></iframe>
           </div>
-          
-          <p className="text-sm text-mute mt-6">{content?.contentAdditionalNotes}</p>
+
+          {/* content section */}
+          <div className="w-full md:w-1/3 md:py-10">
+            <h1 className="text-2xl md:text-4xl font-bold">{content?.contentTitle}</h1>
+
+            <div className="flex flex-wrap gap-2 mt-2 md:mt-4">
+              {
+                content?.Tags.map((tag) => (
+                  <Tag key={tag} tag={tag} />
+                ))
+              }
+            </div>
+            
+            <p className="text-sm text-mute mt-6">{content?.contentAdditionalNotes}</p>
+          </div>
         </div>
-      </div>
 
 
-      {/* related content section */}
-      <div className="mt-16">
-        <HighliteTitle title={'Related'} hightliteText={'Contents'} />
-        <div className="contents">
-          <RelatedContentSection tag={content?.Tags} contentId={content?.contentId}/>
+        {/* related content section */}
+        <div className="mt-16">
+          <HighliteTitle title={'Related'} hightliteText={'Contents'} />
+          <div className="contents">
+            <RelatedContentSection tag={content?.Tags} contentId={content?.contentId}/>
+          </div>
         </div>
+        
       </div>
-      
-    </div>
+
+    ) : (
+      <>
+
+        {/* Content not found error message */}
+        <ContentNotFoundError />
+
+        {/* related content section */}
+        <div className="mt-16">
+          <HighliteTitle title={'Related'} hightliteText={'Contents'} />
+          <div className="contents">
+            <RelatedContentSection tag={content?.Tags} contentId={content?.contentId}/>
+          </div>
+        </div>
+        
+      </>
+    )
+    
     
   );
 };
--- src/components/layouts/YoutubeContentCard/YoutubeContentCard.jsx
+++ src/components/layouts/YoutubeContentCard/YoutubeContentCard.jsx
@@ -4,8 +4,6 @@ import Link from "next/link";
 import React from "react";
 
 const YoutubeContentCard = ({ content }) => {
-
-  console.log(content);
    
   return (
     content?.contentLink &&
